e338bf9f19d3b4b936552934c49652143de1a004
rbverissimo <rverissimo.dev@gmail.com>
2023-01-31 20:39:03 -0300

    Bug fix on application.xml. Implemented on PersonServices all the CRUD methods extracted from the PersonRepository. Made some changes on the controller

--- src/main/java/com/rvrsmo/services/PersonServices.java
+++ src/main/java/com/rvrsmo/services/PersonServices.java
@@ -1,20 +1,19 @@
 package com.rvrsmo.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.rvrsmo.exception.ResourceNotFoundException;
 import com.rvrsmo.model.Person;
 import com.rvrsmo.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
 	
-	private final AtomicLong counter = new AtomicLong();
+	
 	private Logger logger = Logger
 			.getLogger(PersonServices.class.getName());
 	
@@ -25,50 +24,60 @@ public class PersonServices {
 	
 	public Person create(Person person) {
 		logger.info("Creating one person!");
-		return person; 
+		return repository.save(person); 
 	}
 	
 	public Person update(Person person) {
 		logger.info("Updating one person!");
-		return person; 
+		
+		Person entity = repository.findById(person.getId())
+			.orElseThrow(() ->
+				new ResourceNotFoundException("No records found for this ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(person); 
 	}
 	
-	public void delete(String id) {
+	public void delete(Long id) {
 		logger.info("Deleting one person!"); 
+		
+		Person entity = repository.findById(id)
+				.orElseThrow(() ->
+					new ResourceNotFoundException("No records found for this ID"));
+		repository.delete(entity);
 	}
 	
 	
 	public List<Person> findAll() {
-		List<Person> persons = new ArrayList<>(); 
 		
-		logger.info("Finding all people!!!");
-		for (int i = 0; i < 8; i++) {
-			Person person = mockPerson(i); 
-			persons.add(person);
-			
-		}
-		
-		return persons; 
+		logger.info("Finding all people");
+		return repository.findAll();
 	}
 	
 
-	public Person findById(String id) {
+	public Person findById(Long id) {
 		logger.info("Finding one person!");
 		
 		Person person = new Person();
-		person.setId(counter.incrementAndGet());
 		person.setFirstName("Leandro");
 		person.setLastName("Costa");
 		person.setAddress("Uberlândia - MG");
 		person.setGender("male");
 		
-		return person; 
+		return repository.findById(id)
+				.orElseThrow(() -> 
+					new ResourceNotFoundException("No records found for this ID")); 
 	}
 	
 	
+	@SuppressWarnings("unused")
 	private Person mockPerson(int i) {
 		Person person = new Person();
-		person.setId(counter.incrementAndGet());
+		// person.setId(counter.incrementAndGet());
 		person.setFirstName("Person name" + i);
 		person.setLastName("Last name" + i);
 		person.setAddress("Address" + i);
