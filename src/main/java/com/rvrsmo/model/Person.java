a3187a04060f585c3de646f17e72d09b643b8f8d
rbverissimo <rverissimo.dev@gmail.com>
2023-01-31 18:41:24 -0300

    Setting the data mapping for the Person class and also creating the JpaRepository that will enable our CRUD operations

--- src/main/java/com/rvrsmo/model/Person.java
+++ src/main/java/com/rvrsmo/model/Person.java
@@ -3,7 +3,11 @@ package com.rvrsmo.model;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
@@ -13,10 +17,20 @@ public class Person implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name="first_name", nullable = false, length = 80)
 	private String firstName;
-	private String lastName; 
+	
+	@Column(name="last_name", nullable = false, length = 80)
+	private String lastName;
+	
+	@Column(nullable = false, length = 100)
 	private String address;
+	
+	@Column(nullable = false, length = 6)
 	private String gender;
 	
 	public Person() {}
