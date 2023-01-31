8067e1e64f03fb20bfcee41690ca72dbdcd15d2a
rbverissimo <rverissimo.dev@gmail.com>
2023-01-31 20:46:00 -0300

    Refactoring the Delete method. Now it returns a ResponseEntity<?> with noContent.build. That makes the response to 204 NO CONTENT instead of 200 OK

--- src/main/java/com/rvrsmo/repositories/PersonRepository.java
+++ src/main/java/com/rvrsmo/repositories/PersonRepository.java
@@ -1,9 +1,7 @@
 package com.rvrsmo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.rvrsmo.model.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long>{}
