0714e77f0e32bf35f78b1633fcaf95b7a5996498
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2023-09-06 06:55:22 -0300

    Implementação do Dozer mapper para o mapeamento de POJOs, DTOs e VOs para as classes que lidarão com o acesso ao banco de dados.

--- src/main/java/com/rvrsmo/repositories/PersonRepository.java
+++ src/main/java/com/rvrsmo/repositories/PersonRepository.java
@@ -2,6 +2,6 @@ package com.rvrsmo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.rvrsmo.data.vo.v1.PersonVO;
+import com.rvrsmo.model.Person;
 
-public interface PersonRepository extends JpaRepository<PersonVO, Long>{}
+public interface PersonRepository extends JpaRepository<Person, Long>{}
