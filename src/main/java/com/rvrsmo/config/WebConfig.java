ea274391d26efa5ecbde376270b92cda51aa9075
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2023-09-15 06:44:51 -0300

    Preparando a arquitetura para aceitar MediaTypes YAML.

--- src/main/java/com/rvrsmo/config/WebConfig.java
+++ src/main/java/com/rvrsmo/config/WebConfig.java
@@ -8,15 +8,28 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	// VIA QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
 	@Override
+	/*
+	 * public void configureContentNegotiation(ContentNegotiationConfigurer
+	 * configurer) { configurer.favorParameter(true)
+	 * .parameterName("mediaType").ignoreAcceptHeader(true)
+	 * .useRegisteredExtensionsOnly(false)
+	 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
+	 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML); }
+	 */
+	
+	
+	// VIA HEADER PARAM
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(true)
-			.parameterName("mediaType").ignoreAcceptHeader(true)
+		configurer.favorParameter(false)
+			.ignoreAcceptHeader(false)
 			.useRegisteredExtensionsOnly(false)
 			.defaultContentType(MediaType.APPLICATION_JSON)
 			.mediaType("json", MediaType.APPLICATION_JSON)
 			.mediaType("xml", MediaType.APPLICATION_XML);
 	}
+	
 	
 	
 
---
+++ src/main/java/com/rvrsmo/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -0,0 +1,15 @@
+package com.rvrsmo.serialization.converter;
+
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+	protected YamlJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+		super(objectMapper);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
