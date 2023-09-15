9d8cbde4e195697927e3cda0949e806437263fe1
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2023-09-15 06:58:39 -0300

    Implementando o arquivo de serialização que vai fazer a conversão para Yaml

--- src/main/java/com/rvrsmo/serialization/converter/YamlJackson2HttpMessageConverter.java
+++ src/main/java/com/rvrsmo/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -1,13 +1,17 @@
 package com.rvrsmo.serialization.converter;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
 
 	protected YamlJackson2HttpMessageConverter(ObjectMapper objectMapper) {
-		super(objectMapper);
+		super(new YAMLMapper()
+				.setSerializationInclusion(
+						JsonInclude.Include.NON_NULL), MediaType.parseMediaTypes("application/x-yaml"));
 		// TODO Auto-generated constructor stub
 	}
 	
