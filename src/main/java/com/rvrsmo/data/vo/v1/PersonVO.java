034e71b71f07f162d3927625badd064be7599a70
renatoFromRyzen7 <rverissimo.dev@gmail.com>
2023-09-14 21:55:41 -0300

    Adicionando Json Custom

--- src/main/java/com/rvrsmo/data/vo/v1/PersonVO.java
+++ src/main/java/com/rvrsmo/data/vo/v1/PersonVO.java
@@ -3,16 +3,27 @@ package com.rvrsmo.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"})
 public class PersonVO implements Serializable {
 
 	
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;	
+	
+	@JsonProperty("first_name")
 	private String firstName;	
+	
+	@JsonProperty("last_name")
 	private String lastName;	
 	private String address;	
+	
+	// @JsonIgnore
 	private String gender;
 	
 	public PersonVO() {}
