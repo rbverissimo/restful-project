e708c39bc42c4e20e0d3b0c33faf84c802d69484
rbverissimo <rverissimo.dev@gmail.com>
2023-09-20 06:54:50 -0300

    Adicionando o import ao com.fasterxml.jackson.dataformat.yaml.YAMLMapper;

--- src/main/java/com/rvrsmo/serialization/converter/YamlJackson2HttpMessageConverter.java
+++ src/main/java/com/rvrsmo/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -5,17 +5,19 @@ import org.springframework.http.converter.json.AbstractJackson2HttpMessageConver
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 
 public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
 
 	protected YamlJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+		
 		/*
 		 * super(new YAMLMapper() .setSerializationInclusion(
 		 * JsonInclude.Include.NON_NULL),
 		 * MediaType.parseMediaTypes("application/x-yaml"));
 		 */
-		super(objectMapper);
+		 super(objectMapper);
 		// TODO Auto-generated constructor stub
 	}
 	
