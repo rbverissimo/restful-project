648797a497452518d2f0ede74bdf3389dee46f1c
rbverissimo <rverissimo.dev@gmail.com>
2023-09-23 10:06:00 -0300

    Resolvendo problemas ligados ao MessageConverters passando uma instancia do YamlJackson2HttpMessageConverter

--- src/main/java/com/rvrsmo/config/WebConfig.java
+++ src/main/java/com/rvrsmo/config/WebConfig.java
@@ -1,15 +1,29 @@
 package com.rvrsmo.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.rvrsmo.serialization.converter.YamlJackson2HttpMessageConverter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
 	private static MediaType MEDIA_TYPE_APPLICATION_YAML =
 			MediaType.valueOf("application/x-yaml");
+	
+	
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+
+
 
 	// VIA QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
 	@Override
--- src/main/java/com/rvrsmo/serialization/converter/YamlJackson2HttpMessageConverter.java
+++ src/main/java/com/rvrsmo/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -4,13 +4,12 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 
 public class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
 
-	protected YamlJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+	public YamlJackson2HttpMessageConverter() {
 		
 		
 		  super(new YAMLMapper().setSerializationInclusion
