d00afa68dafa364d9caba8a2b0c84e11142bbaed
rbverissimo <rverissimo.dev@gmail.com>
2023-09-26 22:42:51 -0300

    commit da amizade

---
+++ src/test/java/com/rvrsmo/unittests/mockito/services/PersonServicesTest.java
@@ -0,0 +1,54 @@
+package com.rvrsmo.unittests.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.rvrsmo.services.PersonServices;
+import com.rvrsmo.unittests.mapper.mocks.MockPerson;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServicesTest {
+	
+	MockPerson input;
+	
+	@InjectMocks
+	private PersonServices service;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+}
